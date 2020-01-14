1796bb0d0d6349bceccf770e1dd14e11f0f3f2d6
Arif Fadly Siregar <fadlyarif77@gmail.com>
2020-01-14 22:59:23 +0700

    change url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/wisudapcr';
-$config['base_url'] = 'http://pawisuda.riset.pcr.ac.id';
+$config['base_url'] = 'http://pawisuda.pocari.id';
 
 /*
 |--------------------------------------------------------------------------
